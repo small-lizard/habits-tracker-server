d6ce62852a1a81255a10ef47d1dabba3347d96aa
small-lizard <a.kulabukhova001@gmail.com>
2025-12-19 00:07:32 +0400

    chore: Prepared project for deployment

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
-import {UserModel, User} from './models/user.model';
-import {HabitModel, Habit} from './models/habit.model';
-import UserController from './controllers/UserController';
-import HabitController from './controllers/HabitController';
-import UserRepository from './repositories/UserRepository'; // убрать дефолт
-import HabitRepository from './repositories/HabitRepository';
-import MongoRepository from './repositories/MongoRepository';
+import { UserModel, User } from '@models/user.model.js';
+import { HabitModel, Habit } from '@models/habit.model.js';
+import { UserController } from '@controllers/UserController.js';
+import { HabitController } from '@controllers/HabitController.js';
+import { UserRepository } from '@repositories/UserRepository.js';
+import { HabitRepository } from '@repositories/HabitRepository.js';
+import { MongoRepository } from '@repositories/MongoRepository.js';
 import MongoStore from 'connect-mongo';
 import session from 'express-session';
 import dotenv from 'dotenv';
-import { requireAuth } from './middlewares/authMiddleware';
+import { requireAuth } from '@middlewares/authMiddleware.js';
 dotenv.config();
 
 const app = express();
@@ -22,15 +22,15 @@ const host = process.env.RENDER_EXTERNAL_URL ?? 'http://localhost';
 const userRepository = new UserRepository(new MongoRepository<User>(UserModel));
 const habitRepository = new HabitRepository(new MongoRepository<Habit>(HabitModel));
 
-const userController = new UserController({ 
-    userRepository, 
-    habitRepository 
+const userController = new UserController({
+  userRepository,
+  habitRepository
 });
 
 const habitController = new HabitController({ habitRepository });
 
 app.use(cors({
-  origin: process.env.CLIENT_URL,
+  origin: 'https://habits-tracker-dusky.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type'],
   credentials: true
@@ -44,6 +44,8 @@ if (!secret) {
   throw new Error('Secret not provided')
 }
 
+app.set('trust proxy', 1);
+
 app.use(session({
   secret,
   resave: false,
@@ -54,23 +56,23 @@ app.use(session({
   }),
   cookie: {
     maxAge: 1000 * 60 * 60 * 24 * 3,
-    httpOnly: true,
-    sameSite: 'lax',
-    secure: false
+    secure: true,
+    sameSite: 'none',
+    httpOnly: true
   },
   rolling: true,
 }))
 
-if(!mongoUrl) {
-  throw new Error ('Incorrect url for MongoDB')
+if (!mongoUrl) {
+  throw new Error('Incorrect url for MongoDB')
 }
 
-if(!host) {
-  throw new Error ('Incorrect host url')
+if (!host) {
+  throw new Error('Incorrect host url')
 }
 
-if(!port) {
-  throw new Error ('Incorrect port')
+if (!mongoUrl) {
+  throw new Error('Incorrect port')
 }
 
 mongoose.connect(mongoUrl)
@@ -83,17 +85,16 @@ app.post('/auth', userController.addUser);
 app.get('/auth/check', userController.checkIsAuth);
 app.post('/login', userController.login);
 app.post('/logout', userController.logout);
-app.put('/change-password',requireAuth, userController.changePassword);
-app.delete('/delete-account',requireAuth, userController.delete);
+app.put('/change-password', requireAuth, userController.changePassword);
+app.delete('/delete-account', requireAuth, userController.delete);
 
 // HABIT ROUTES
-app.post('/habits/add',requireAuth, habitController.addHabit)
-app.post('/habits/update',requireAuth, habitController.updateHabit)
-app.delete('/habits/delete/:id',requireAuth, habitController.delete);
-app.post('/habits/sync',requireAuth, habitController.sync);
-app.get('/habits',requireAuth, habitController.getHabits);
+app.post('/habits/add', requireAuth, habitController.addHabit)
+app.post('/habits/update', requireAuth, habitController.updateHabit)
+app.delete('/habits/delete/:id', requireAuth, habitController.delete);
+app.get('/habits', requireAuth, habitController.getHabits);
 
-app.listen(port, host, () => {
+
+app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
